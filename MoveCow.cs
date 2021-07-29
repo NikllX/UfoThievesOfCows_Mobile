02946bac64125c7b1b3b829cbd11c2e03a801f28
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 17:33:40 +0300

    Движение коровы

---
+++ MoveCow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveCow : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public Vector2 direction;
+    void Update()
+    {
+        transform.Translate(direction.normalized * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Zone")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
